1017bb938b8216e4545ff147f4b64787f7a481da
altart <pedromendescraft@gmail.com>
2021-08-07 18:31:49 -0300

    Token page

---
+++ token/script.js
@@ -0,0 +1,28 @@
+function getUrlVars() {
+    var vars = {};
+    window.location.href.replace(/[#&]+([^=&]+)=([^&]*)/gi, (m,key,value) => {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+let token = getUrlVars()["access_token"];
+
+
+let inputToken = document.querySelector("#token");
+if (token == undefined) {
+    let divToken = document.querySelector(".token");
+    divToken.innerHTML = "Login failed";
+} else {
+    inputToken.value = token;
+}
+
+let inputCopy = document.querySelector("#copy");
+
+if (inputCopy != undefined) {
+    inputCopy.addEventListener("click", ()=>{
+        inputToken.select();
+        inputToken.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+    })
+}
