06ddad23839107569ab9e9862268f733591ccfe4
alt-art <pedromendescraft@gmail.com>
2021-08-15 18:31:59 -0300

    Error handling and redirection

--- token/script.js
+++ token/script.js
@@ -7,14 +7,23 @@ function getUrlVars() {
 }
 
 let token = getUrlVars()["access_token"];
+let errorDescription = getUrlVars()["error_description"];
 let inputCopy = document.querySelector("#copy");
 let msg = document.querySelector(".token-message");
+let errorDescriptionParagraph = document.querySelector("#error_description");
 
 if (token == undefined) {
     msg.innerText = "Login failed";
-    inputCopy.style.display = "none"
+    if (errorDescription != undefined) {
+        errorDescriptionParagraph.innerText = errorDescription.split("+").join(" ");
+    } else {
+        window.location.replace("../");
+    }
+} else {
+    msg.innerText = "Paste the token on terminal";
+    inputCopy.style.display = "flex";
 }
 
 inputCopy.addEventListener("click", async () => {
-    await navigator.clipboard.writeText(token)
+    await navigator.clipboard.writeText(token);
 })
